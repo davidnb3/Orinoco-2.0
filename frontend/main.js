03055b705f3bd192207be6601f752d00edf4759a
kosmical <david.mario.pieri@gmail.com>
2021-02-01 12:26:00 +0100

    updated main.js

--- frontend/main.js
+++ frontend/main.js
@@ -1,62 +1,72 @@
 function makeRequest() {
-    return new Promise((resolve, reject) => {
-      let request = new XMLHttpRequest();
-      request.open('GET', "http://localhost:3000/api/cameras");
-      request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-          if(request.status === 200) {
-            resolve(JSON.parse(request.response));
-          } else {
-            reject('Sorry, something went wrong.');
-          }
+  return new Promise((resolve, reject) => {
+    let request = new XMLHttpRequest();
+    request.open('GET', "http://localhost:3000/api/teddies");
+    request.onreadystatechange = () => {
+      if (request.readyState === 4) {
+        if(request.status === 200 || request.status === 201) {
+          resolve(JSON.parse(request.response));
+        } else {
+          reject('Sorry, something went wrong.');
         }
       }
-      request.send();
-    });
-  };
-  
-  async function requestPromise() {
-    try {
-      const promiseRequest = makeRequest();
-      const promiseResponse = await promiseRequest;
-      addItemImages(promiseResponse);
-      addItemNames(promiseResponse);
-      addItemPrices(promiseResponse);
-      addItemDescription(promiseResponse);
-    } catch (error) {
-      
     }
-  };
-  
-  requestPromise();
+    request.send();
+  });
+};
 
-  function addItemImages(response) {
-    const images = document.querySelectorAll('.card-img-top');
-    for (let i in response) {
-        let imgSrc = response[i].imageUrl;
-        images[i].setAttribute('src', imgSrc);
-    }
+async function showItems() {
+  try {
+    const promiseRequest = makeRequest();
+    const promiseResponse = await promiseRequest;
+    addItemImages(promiseResponse);
+    addItemNames(promiseResponse);
+    addItemPrices(promiseResponse);
+    addItemDescription(promiseResponse);
+    addItemLinks(promiseResponse);
+  } catch (error) {
+    
   }
+};
+
+showItems();
+
+function addItemImages(response) {
+  const images = document.querySelectorAll('.card-img-top');
+  for (let i in response) {
+      let imgSrc = response[i].imageUrl;
+      images[i].setAttribute('src', imgSrc);
+  }
+}
 
 function addItemNames(response) {
-    const names = document.querySelectorAll('.item-name');
-    for (let i in response) {
-        names[i].textContent = response[i].name;
-    }
+  const names = document.querySelectorAll('.item-name');
+  for (let i in response) {
+      names[i].textContent = response[i].name;
+  }
 
 }
 
 function addItemPrices(response) {
-    const prices = document.querySelectorAll('.item-price');
-    for (let i in response) {
-        prices[i].textContent = '$' + response[i].price / 100;
-    }
+  const prices = document.querySelectorAll('.item-price');
+  for (let i in response) {
+      prices[i].textContent = '$' + response[i].price / 100;
+  }
 }
 
 function addItemDescription(response) {
-    const descriptions = document.querySelectorAll('.card-text');
-    for (let i in response) {
-        descriptions[i].textContent = response[i].description;
-    } 
+  const descriptions = document.querySelectorAll('.card-text');
+  for (let i in response) {
+      descriptions[i].textContent = response[i].description;
+  } 
+}
+
+function addItemLinks(response) {
+  const imageLinks = document.querySelectorAll('.image-link');
+  const nameLinks = document.querySelectorAll('.name-link');
+  for (let i in response) {
+      imageLinks[i].setAttribute('href', 'product.html?id=' + response[i]._id)
+      nameLinks[i].setAttribute('href', 'product.html?id=' + response[i]._id)
+  }
 }
 
